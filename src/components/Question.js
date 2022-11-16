840ab1bab1977f75e1423dde61d712dad4f647a3
Anthony <amartin7789@gmail.com>
2022-11-16 14:03:15 -0700

    imported quizzical files

---
+++ src/components/Question.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Question({question}) {
+    
+    return (
+        <div className="single-question">
+            <h3>{question}</h3>
+        </div>
+    )
+}
